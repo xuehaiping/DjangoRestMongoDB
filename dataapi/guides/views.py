3f29450c83370b7ecc1ebe767474a110ba4f3ef1
xuehaiping <claymoresq@gmail.com>
2018-03-12 22:54:09 -0500

    Data injection succeeded.

--- dataapi/guides/views.py
+++ dataapi/guides/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+from guides.models import Guide, Process
 
-# Create your views here.
+
+def index(request):
+    p1 = Process(text='First Process ever', listOfImage=['url1', 'url2'])
+    p2 = Process(text='Second Process ever', listOfImage=['url21', 'url22'])
+
+    g1 = Guide(title='firstGuide',
+               authorId='12345',
+               listOfProcess=[p1, p2],
+               dateTime=datetime.datetime.now(),
+               listOfTag=['tag1', 'tag2'],
+               listOfCategory=['cat1', 'cat2'])
+
+    g1.save()
+    return HttpResponse(str(g1))
